7bee09308ef410f1b713d8359506e754bf5fa862
andreas kouvousis <andreaskoub@gmail.com>
2020-09-04 01:44:28 +0300

    !GAMING

--- disbot.py
+++ disbot.py
@@ -67,7 +67,7 @@ async def on_message(message):
 		else:
 			await message.channel.send("RL")
 			
-	if message.content.startswith("!gaming"):
+	if message.content.startswith("!GAMING"):
 		three_hours_from_now = (datetime.now() + timedelta(hours=3)).strftime('%H:%M:%S')
 		await message.channel.send("Θα παιξεις μεχρι " + three_hours_from_now + ".")
 
